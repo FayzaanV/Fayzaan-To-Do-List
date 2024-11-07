763c0e1c5cc3b33673452383e45f725ae99570b2
EvanJ <122122197+FayzaanV@users.noreply.github.com>
2024-11-07 15:15:27 -0600

    End of 11/7/24

--- JS/todo.js
+++ JS/todo.js
@@ -1,37 +1,36 @@
 document.getElementById("addItemButton").addEventListener("click", function () {
-  const itemInput = document.getElementById("itemInput");
-  const dateInput = document.getElementById("dateInput");
-  const todoList = document.getElementById("todoList");
+    const itemInput = document.getElementById("itemInput");
+    const dateInput = document.getElementById("dateInput");
+    const todoList = document.getElementById("todoList");
 
-  const itemText = itemInput.value;
-  const dateText = dateInput.value;
+    const itemText = itemInput.value;
+    const dateText = dateInput.value;
 
-  if (itemText === "" || dateText === "") {
-    alert("Please enter both an item and a date.");
-    return;
-  }
+    if (itemText === "" || dateText === "") {
+        alert("Please enter both an item and a date.");
+        return;
+    }
 
-  // Parse and format the date
-  const date = new Date(dateText);
-  const formattedDate = `${
-    date.getMonth() + 1
-  }/${date.getDate()}/${date.getFullYear()}`;
+    // Parse and format the date
+    const date = new Date(dateText);
+    const formattedDate = `${date.getMonth() + 1
+        }/${date.getDate()}/${date.getFullYear()}`;
 
-  const listItem = document.createElement("div");
-  listItem.className = "todo-item";
-  listItem.innerHTML = `
-          <span>${itemText} - ${formattedDate}</span>
-          <button class="deleteButton">Delete</button>
+    const listItem = document.createElement("div");
+    listItem.className = "todo-item";
+    listItem.innerHTML = `
+          <div class="thing"><span class="items">${itemText} - ${formattedDate}</span>
+          <button class="deleteButton">Delete</button></div>
       `;
 
-  todoList.appendChild(listItem);
+    todoList.appendChild(listItem);
 
-  itemInput.value = "";
-  dateInput.value = "";
+    itemInput.value = "";
+    dateInput.value = "";
 
-  listItem
-    .querySelector(".deleteButton")
-    .addEventListener("click", function () {
-      todoList.removeChild(listItem);
-    });
+    listItem
+        .querySelector(".deleteButton")
+        .addEventListener("click", function () {
+            todoList.removeChild(listItem);
+        });
 });
